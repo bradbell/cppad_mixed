8723dfd85bcc8449c7f6826bbb7523dbb3cadcce
Brad Bell <bradbell@seanet.com>
2015-12-07 09:13:42 -0800

    dismod_at:include/dismod_at/chol_hes_ran.hpp -> include/cppad_mixed/chol_hes_ran.hpp

---
+++ include/cppad_mixed/chol_hes_ran.hpp
@@ -0,0 +1,41 @@
+// $Id:$
+/* --------------------------------------------------------------------------
+dismod_at: Estimating Disease Rates as Functions of Age and Time
+          Copyright (C) 2014-15 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+# ifndef DISMOD_AT_CHOL_HES_RAN_HPP
+# define DISMOD_AT_CHOL_HES_RAN_HPP
+
+# include <Eigen/Sparse>
+
+namespace dismod_at {
+	// This would be member of cppad_mixed class if it were not for all the
+	// warnings Eigen generates.
+	extern Eigen::SimplicialLDLT<
+		Eigen::SparseMatrix<double> , Eigen::Lower
+	> chol_hes_ran_;
+
+	extern void analyze_chol_hes_ran(
+		size_t                       n_fixed  ,
+		size_t                       n_random ,
+		const CppAD::vector<size_t>& row      ,
+		const CppAD::vector<size_t>& col
+	);
+	extern void factorize_chol_hes_ran(
+		size_t                       n_fixed  ,
+		size_t                       n_random ,
+		const CppAD::vector<size_t>& row      ,
+		const CppAD::vector<size_t>& col      ,
+		const CppAD::vector<double>& both     ,
+		CppAD::ADFun<double>&        hessian
+	);
+	extern double logdet_chol_hes_ran(size_t n_random);
+}
+
+
+# endif
