43cbe8a8bc519fa5b65913fe742337d6af1a8bbf
Brad Bell <bradbell@seanet.com>
2015-12-07 09:13:43 -0800

    dismod_at:include/dismod_at/mixed_unpack.hpp -> include/cppad_mixed/mixed_unpack.hpp

---
+++ include/cppad_mixed/mixed_unpack.hpp
@@ -0,0 +1,142 @@
+// $Id:$
+/* --------------------------------------------------------------------------
+dismod_at: Estimating Disease Rates as Functions of Age and Time
+          Copyright (C) 2014-15 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+# ifndef DISMOD_AT_MIXED_UNPACK_HPP
+# define DISMOD_AT_MIXED_UNPACK_HPP
+# include <dismod_at/cppad_mixed.hpp>
+# include <dismod_at/a2_double.hpp>
+
+/*
+$begin cppad_mixed_unpack$$
+$spell
+	cppad
+	vec
+	const
+	Cpp
+	dismod
+	hpp
+$$
+
+$section cppad_mixed: Pack Fixed Effect and Random Effects Into One Vector$$
+
+$head Syntax$$
+$codei%unpack(%fixed_one%, %random_vec%, %both_vec%)
+%$$
+$codei%unpack(%fixed_one%, %fixed_two%, %random_vec%, %three_vec%)
+%$$
+
+$head Private$$
+This function is $code private$$ to the $code cppad_mixed$$ class
+and cannot be used by a derived
+$cref/mixed_object/cppad_mixed_derived_ctor/mixed_object/$$.
+
+$head Float_pack$$
+This can be any type.
+
+$head Float_unpack$$
+If $icode x$$ has type $icode Float_pack$$,
+the syntax $icode%Float_unpack%(%x%)%$$ must convert $icode x$$
+to the type $icode Float_unpack$$.
+
+$head fixed_one$$
+This argument has prototype
+$codei%
+	CppAD::vector<%Float_unpack%>& %fixed_one%
+%$$
+The input value of its elements does not matter.
+Upon return, it contains the value of the first
+$cref/fixed effects/cppad_mixed/Fixed Effects, theta/$$.
+The size of this vector must be equal to
+$cref/n_fixed_/cppad_mixed_private/n_fixed_/$$.
+
+$head fixed_two$$
+This argument has prototype
+$codei%
+	CppAD::vector<%Float_unpack%>& %fixed_two%
+%$$
+The input value of its elements does not matter.
+Upon return, it contains the value of the second
+$cref/fixed effects/cppad_mixed/Fixed Effects, theta/$$.
+The size of this vector must be equal to
+$cref/n_fixed_/cppad_mixed_private/n_fixed_/$$.
+
+$head random_vec$$
+This argument has prototype
+$codei%
+	CppAD::vector<%Float_unpack%>& %random_vec%
+%$$
+The input value of its elements does not matter.
+Upon return, it contains the value of the
+$cref/random effects/cppad_mixed/Random Effects, u/$$.
+The size of this vector must be equal to
+$cref/n_random_/cppad_mixed_private/n_random_/$$.
+
+$head both_vec$$
+This argument has prototype
+$codei%
+	const CppAD::vector<%Float_pack%>& %both_vec%
+%$$
+If present, the size of this vector must be equal to
+$icode%n_fixed_% + %n_random_%$$.
+It contains the fixed effect and random effects as one vector.
+
+$head three_vec$$
+This argument has prototype
+$codei%
+	const CppAD::vector<%Float_pack%>& %three_vec%
+%$$
+If present, the size of this vector must be equal to
+$codei%2*%n_fixed_% + %n_random_%$$.
+It contains the first fixed effects vector,
+the second fixed effects vector,
+and random effects as one vector.
+
+$end
+*/
+namespace dismod_at { // BEGIN_DISMOD_AT_NAMESPACE
+
+template <class Float_unpack, class Float_pack>
+void cppad_mixed::unpack(
+	CppAD::vector<Float_unpack>&      fixed_one  ,
+	CppAD::vector<Float_unpack>&      random_vec ,
+	const CppAD::vector<Float_pack>&  both_vec   ) const
+{
+	assert( fixed_one.size() == n_fixed_ );
+	assert( random_vec.size() == n_random_ );
+	assert( both_vec.size() == n_fixed_ + n_random_ );
+	for(size_t j = 0; j < n_fixed_; j++)
+		fixed_one[j] = Float_unpack( both_vec[j] );
+	for(size_t j = 0; j < n_random_; j++)
+		random_vec[j] = Float_unpack( both_vec[n_fixed_ + j] );
+}
+
+template <class Float_unpack, class Float_pack>
+void cppad_mixed::unpack(
+	CppAD::vector<Float_unpack>&      fixed_one  ,
+	CppAD::vector<Float_unpack>&      fixed_two  ,
+	CppAD::vector<Float_unpack>&      random_vec ,
+	const CppAD::vector<Float_pack>&  three_vec  ) const
+{
+	assert( fixed_one.size() == n_fixed_ );
+	assert( fixed_two.size() == n_fixed_ );
+	assert( random_vec.size() == n_random_ );
+	assert( three_vec.size() == 2 * n_fixed_ + n_random_ );
+	for(size_t j = 0; j < n_fixed_; j++)
+	{	fixed_one[j] = Float_unpack( three_vec[j] );
+		fixed_two[j] = Float_unpack( three_vec[n_fixed_ + j] );
+	}
+	for(size_t j = 0; j < n_random_; j++)
+		random_vec[j] = Float_unpack( three_vec[2 * n_fixed_ + j] );
+}
+
+
+} // END_DISMOD_AT_NAMESPACE
+
+# endif
