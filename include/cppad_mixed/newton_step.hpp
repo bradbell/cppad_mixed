d160572f5ef29f8539631e3b5a9532068eb3106a
Brad Bell <bradbell@seanet.com>
2015-12-07 09:13:43 -0800

    dismod_at:include/dismod_at/newton_step.hpp -> include/cppad_mixed/newton_step.hpp

---
+++ include/cppad_mixed/newton_step.hpp
@@ -0,0 +1,62 @@
+// $Id$
+/* --------------------------------------------------------------------------
+dismod_at: Estimating Disease Rates as Functions of Age and Time
+          Copyright (C) 2014-15 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+# ifndef DISMOD_AT_NEWTON_STEP_HPP
+# define DISMOD_AT_NEWTON_STEP_HPP
+
+# include <cppad/cppad.hpp>
+
+namespace dismod_at { // BEGIN_DISMOD_AT_NAMESPACE
+
+
+class newton_step_algo {
+	typedef CppAD::AD<double>            a1_double;
+	typedef CppAD::vector<a1_double>     a1d_vector;
+private:
+		const size_t                      n_fixed_;
+		const size_t                      n_random_;
+		// set by constructor
+		CppAD::ADFun<a1_double>&          a1_adfun_;
+		CppAD::vector<size_t>             row_;
+		CppAD::vector<size_t>             col_;
+		CppAD::sparse_hessian_work        work_;
+public:
+	newton_step_algo(
+		CppAD::ADFun<a1_double>&          a1_adfun   ,
+		const CppAD::vector<double>&      theta      ,
+		const CppAD::vector<double>&      u
+	);
+	void operator()(
+		const a1d_vector& a1_theta_u_v   ,
+		a1d_vector&       a1_logdet_step
+	);
+};
+
+class newton_step {
+	typedef CppAD::AD<double>             a1_double;
+	typedef CppAD::vector<a1_double>      a1d_vector;
+private:
+	CppAD::checkpoint<double>*            atom_fun_;
+public:
+	newton_step(void);
+	~newton_step(void);
+	void initialize(
+		CppAD::ADFun<a1_double>&          a1_adfun   ,
+		const CppAD::vector<double>&      fixed_vec  ,
+		const CppAD::vector<double>&      random_vec
+	);
+	size_t size_var(void);
+	void eval(const a1d_vector& a1_theta_u_v, a1d_vector& a1_logdet_step);
+};
+
+
+} // END_DISMOD_AT_NAMESPACE
+
+# endif
