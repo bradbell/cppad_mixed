446982fe1327f82eaa749fd672a1aedd4eb34a30
Brad Bell <bradbell@seanet.com>
2016-01-04 12:46:33 -0700

    Note that example overides the default warning and fatal error handlers.

--- example/user/derived_xam.cpp
+++ example/user/derived_xam.cpp
@@ -1,7 +1,7 @@
 // $Id$
 /* --------------------------------------------------------------------------
 cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-          Copyright (C) 2014-15 University of Washington
+          Copyright (C) 2014-16 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -112,11 +112,13 @@ namespace {
 			const vector<a1_double>& fixed_vec  )
 		{	return implement_fix_likelihood(fixed_vec); }
 		//
+		// example changing fatal error handler
 		virtual void fatal_error(const std::string& error_message)
 		{	std::cerr << "fatal_error = " << error_message << std::endl;
 			std::exit(1);
 		}
 		//
+		// example changing warning handler
 		virtual void warning(const std::string& warning_message)
 		{	std::cerr << "warning = " << warning_message << std::endl;
 		}
