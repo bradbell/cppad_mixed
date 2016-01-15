8267e084a3337f4f5e7fab59d9f8d466b647a496
Brad Bell <bradbell@seanet.com>
2016-01-15 13:29:59 -0700

    batch_edit.sh: first version of a batch editing script.

---
+++ bin/batch_edit.sh
@@ -0,0 +1,83 @@
+#! /bin/bash -e
+# $Id:$
+#  --------------------------------------------------------------------------
+# cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
+#           Copyright (C) 2014-16 University of Washington
+#              (Bradley M. Bell bradbell@uw.edu)
+#
+# This program is distributed under the terms of the
+#	     GNU Affero General Public License version 3.0 or later
+# see http://www.gnu.org/licenses/agpl.txt
+# ---------------------------------------------------------------------------
+new_directories='
+'
+rename_files='
+'
+spell_files='
+'
+no_change_files='
+'
+#
+rename_cmd='s|\(.*\)|\1|'
+spell_cmd='s|^$spell|&|'
+#
+cat << EOF > junk.sed
+EOF
+# -----------------------------------------------------------------------------
+if [ "$0" != "bin/batch_edit.sh" ]
+then
+	echo "bin/batch_edit.sh: must be executed from its parent directory"
+	exit 1
+fi
+# -----------------------------------------------------------------------------
+git reset --hard
+# ----------------------------------------------------------------------------
+# new directories
+for dir in $new_directories
+do
+	if [ ! -e $dir ]
+	then
+		mkdir $dir
+	fi
+done
+# ----------------------------------------------------------------------------
+# rename files
+for old in $rename_files
+do
+	new=`echo $old | sed -e "$rename_cmd"`
+	echo_eval git mv $old $new
+done
+# ----------------------------------------------------------------------------
+list_all=`bin/ls_files.sh`
+for file in $list_all
+do
+	echo_eval sed -f junk.sed -i $file
+done
+# ----------------------------------------------------------------------------
+# files that have spelling changes
+for file in $spell_files
+do
+	sed -e "$spell_cmd" -i $file
+done
+# ----------------------------------------------------------------------------
+# files that should not change at all
+list='
+'
+for file in $list
+do
+	echo_eval git checkout $file
+done
+# ----------------------------------------------------------------------------
+# files that have hand edited and stored using 'git_new.sh to'
+git_new='yes'
+for name in add_list mod_list rm_list
+do
+	if [ ! -e new/$name ]
+	then
+		git_new='no'
+	fi
+done
+if [ "$git_new" == 'yes' ]
+then
+	git_new.sh from
+fi
