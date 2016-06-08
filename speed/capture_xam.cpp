d03639862d7aa4371db7ef8cd6a6998cb574770d
Brad Bell <bradbell@seanet.com>
2016-06-08 16:01:05 -0700

    Switch role of theta[0] and theta[1].

--- speed/capture_xam.cpp
+++ speed/capture_xam.cpp
@@ -65,10 +65,10 @@ $rnext
 $latex u_t$$
 	$cnext random effect for each sampling time
 $rnext
-$latex \theta_0$$
+$latex \theta_1$$
 	$cnext constant term in logit of capture probability
 $rnext
-$latex \theta_1$$
+$latex \theta_0$$
 	$cnext mean for $latex N_i$$ given $latex \theta$$
 	($latex \lambda$$ in reference)
 $rnext
@@ -94,20 +94,20 @@ $cref/reference/capture_xam.cpp/Reference/$$ suggests a
 covariate model for the probability of capture.
 We use a similar model defined by
 $latex \[
-	\R{logit} ( p_{i,t} ) = - u_t - \theta_0
+	\R{logit} ( p_{i,t} ) = - u_t - \theta_1
 \] $$
 It follows that
 $latex \[
-	p_{i,t} = [ 1 + \exp(  u_t + \theta_0 ) ]^{-1}
+	p_{i,t} = [ 1 + \exp(  u_t + \theta_1 ) ]^{-1}
 \] $$
 
 $head Population Probability$$
 We use a Poisson distribution to model the
-probability of $latex N_i$$ given $latex \theta_1$$; i.e.,
+probability of $latex N_i$$ given $latex \theta_0$$; i.e.,
 $latex \[
 \B{P} ( N_i | \theta  )
 =
-\theta_1^{N(i)} \frac{ \exp[ - \theta_1 ] }{ N_i ! }
+\theta_0^{N(i)} \frac{ \exp[ - \theta_0 ] }{ N_i ! }
 \] $$
 We assume that this probability
 is independent for each $latex i$$.
@@ -138,7 +138,7 @@ $latex \[
 \] $$
 We define a probability of capture function for each location and time by
 $latex \[
-p_{i,t} ( \theta , u ) =  [ 1 + \exp( u_t + \theta_0 ) ]^{-1}
+p_{i,t} ( \theta , u ) =  [ 1 + \exp( u_t + \theta_1 ) ]^{-1}
 \] $$
 The binomial likelihood for $latex y_{i, t}$$ given
 $latex N_i$$, $latex \theta$$, and $latex u$$ is
@@ -156,7 +156,7 @@ $latex \[
 L_i ( \theta , u )
 =
 \sum_{k=M(i)}^{2 M(i) + 1}
-	\theta_1^k \frac{ \exp( - \theta_1 ) }{ k ! }
+	\theta_0^k \frac{ \exp( - \theta_0 ) }{ k ! }
 		\prod_{t=0}^{T-1} B_{i,t} ( k , \theta , u )
 \] $$
 We assume that this is a good approximation; i.e.,
@@ -164,7 +164,7 @@ $latex \[
 L_i ( \theta , u )
 \approx
 \sum_{k=M(i)}^{+\infty}
-	\theta_1^k \frac{ \exp( - \theta_1 ) }{ k ! }
+	\theta_0^k \frac{ \exp( - \theta_0 ) }{ k ! }
 		\prod_{t=0}^{T-1} B_{i,t} ( k , \theta , u )
 \] $$
 Our model for the likelihood of the data at all the locations,
@@ -272,7 +272,7 @@ void simulate(
 	//
 	// simulate population sizes
 	vector<double> N(I);
-	double mu =  theta[1];
+	double mu =  theta[0];
 	for(size_t i = 0; i < I; i++)
 		N[i] = gsl_ran_poisson(rng, mu );
 	//
@@ -292,7 +292,7 @@ void simulate(
 	for(size_t i = 0; i < I; i++)
 	{	for(size_t t = 0; t < T; t++)
 		{	// probability of capture
-			double ex = exp( u[t] + theta[0] );
+			double ex = exp( u[t] + theta[1] );
 			double p = 1.0 /( 1.0  + ex );
 			y[ i * T + t ] = gsl_ran_binomial(rng, p, N[i]);
 		}
@@ -371,18 +371,18 @@ public:
 		vector<Float> yit_log_p(I_ * T_), log_1p(I_ * T_);
 		for(size_t i = 0; i < I_; i++)
 		{	for(size_t t = 0; t < T_; t++)
-			{	Float ex   = exp( u[t] + theta[0] );
+			{	Float ex   = exp( u[t] + theta[1] );
 				Float    p = one / (one + ex );
 				yit_log_p[ i * T_ + t ]  = Float(y_[i*T_+t]) * log(p + eps);
 				log_1p[i * T_ + t ]      = log(one - p + eps);
 			}
 		}
 		//
-		// log( theta[1]^k * exp( - theta[1] ) / k! )
+		// log( theta[0]^k * exp( - theta[0] ) / k! )
 		vector<Float> log_poisson(K_);
 		for(size_t k = 0; k < K_; k++)
-		{	log_poisson[k] = log( theta[1] + eps ) * Float(k);
-			log_poisson[k] -= theta[1];
+		{	log_poisson[k] = log( theta[0] + eps ) * Float(k);
+			log_poisson[k] -= theta[0];
 			log_poisson[k] -= logfac_[k];
 		}
 		//
@@ -457,8 +457,8 @@ int main(int argc, char *argv[])
 	//
 	size_t T = n_random;
 	vector<double> theta_sim(n_fixed);
-	theta_sim[0] =   0.50;  // constant term in logit model
-	theta_sim[1] =   5.0;   // mean population size
+	theta_sim[1] =   0.50;  // constant term in logit model
+	theta_sim[0] =   5.0;   // mean population size
 	theta_sim[2] =   1.00;  // standard deviation of random effects
 
 	// simulate y
@@ -470,13 +470,13 @@ int main(int argc, char *argv[])
 	vector<double>
 		theta_lower(n_fixed), theta_in(n_fixed), theta_upper(n_fixed);
 	// constant term
-	theta_lower[0] = 0.0;
-	theta_in[0]    =  theta_sim[0] / 2.0;
-	theta_upper[0] = +2.0;
+	theta_lower[1] = 0.0;
+	theta_in[1]    =  theta_sim[1] / 2.0;
+	theta_upper[1] = +2.0;
 	// mean population
-	theta_lower[1] = 1.0;
-	theta_in[1]    = theta_sim[1] / 2.0;
-	theta_upper[1] = 20.0;
+	theta_lower[0] = 1.0;
+	theta_in[0]    = theta_sim[0] / 2.0;
+	theta_upper[0] = 20.0;
 	// standard deviation of random effects
 	theta_lower[2] = 1e-2;
 	theta_in[2]    = theta_sim[2] / 2.0;
