16cfc2fa5e234567c5cf9a2783b05e5a5f7b8291
Brad Bell <bradbell@seanet.com>
2016-07-29 06:21:44 -0700

    More description of how debug and release versions are built.

--- bin/run_cmake.sh
+++ bin/run_cmake.sh
@@ -31,8 +31,11 @@ build_type='debug'
 # &&
 # Note that &code run_cmake.sh&& looks for a &icode%cppad_prefix%.debug%&&
 # and &icode%cppad_prefix%.release%&& and uses them if they are present.
-# Also note that it builds cppad_mixed in &code build.debug&& or
+# Note that it builds cppad_mixed in &code build.debug&& or
 # &code build.release&& depending on &icode build_type&&.
+# Also note that it uses a soft link from
+# &icode cppad_prefix&&  and &code build&& to the corresponding debug
+# and release directories.
 #
 # &head Prefixes&&
 # Prefixes where the required packages are installed:
