bradbell/cppad_mixed

commit dc66212d3b5ea924e98201359d1778a7fdddb94d

Move example/private/<name>_xam.cpp  -> example/private/<name>.cpp      example/user/<name>_xam.cpp     -> example/user/<name>.cpp

bin/check_install.sh
@@ -46,7 +46,7 @@ eigen_prefix="$HOME/prefix/cppad_mixed/eigen"
 # This is the user example that we will compile using
 # the installed version of &code cppad_mixed&&:
 # &codep
-example_file='example/user/no_random_xam.cpp'
+example_file='example/user/no_random.cpp'
 # &&
 #
 # &head build_type&&

commit 419dc9d33d7d646a855fd6304ce9b3c44f6cff40

Fix problem with check_install.sh that started with commit dc66212d3b5.

bin/check_install.sh
@@ -70,7 +70,7 @@ cd build/tmp
 # The following command gets the example name,
 # which is the example file minus the &code .cpp&& at the end:
 # &codep
-example_name=`echo $example_file | sed -e 's|.*/||' -e 's|\.cpp||'`
+example_name=`echo $example_file | sed -e 's|.*/||' -e 's|\.cpp|_xam|'`
 # &&
 #
 # &head main&&