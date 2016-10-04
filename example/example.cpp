bradbell/cppad_mixed

commit dc66212d3b5ea924e98201359d1778a7fdddb94d

Move example/private/<name>_xam.cpp  -> example/private/<name>.cpp      example/user/<name>_xam.cpp     -> example/user/<name>.cpp

example/CMakeLists.txt
@@ -18,56 +18,56 @@ LINK_DIRECTORIES(
 # -----------------------------------------------------------------------------
 ADD_EXECUTABLE(example EXCLUDE_FROM_ALL
 	example.cpp
-	private/cholmod_solve_xam.cpp
-	private/cholmod_solve2_a_xam.cpp
-	private/cholmod_solve2_sim_xam.cpp
-	private/ldlt_cholmod_xam.cpp
-	private/ldlt_eigen_xam.cpp
+	private/cholmod_solve.cpp
+	private/cholmod_solve2_a.cpp
+	private/cholmod_solve2_sim.cpp
+	private/ldlt_cholmod.cpp
+	private/ldlt_eigen.cpp
 	ipopt_xam_run.cpp
-	private/eigen_xam.cpp
-	private/fix_con_eval_xam.cpp
-	private/fix_con_hes_xam.cpp
-	private/fix_con_jac_xam.cpp
-	private/fix_like_eval_xam.cpp
-	private/fix_like_hes_xam.cpp
-	private/fix_like_jac_xam.cpp
-	private/hes_cross_xam.cpp
-	private/ran_hes_fun_xam.cpp
-	private/logdet_jac_xam.cpp
-	private/newton_step_xam.cpp
-	private/ran_con_eval_xam.cpp
-	private/ran_con_jac_xam.cpp
-	private/ran_like_jac_xam.cpp
-	private/ran_obj_eval_xam.cpp
-	private/ran_obj_jac_xam.cpp
-	private/ran_objcon_hes_xam.cpp
-	private/sparse_ad_cholesky_ad_xam.cpp
-	private/sparse_eigen2info_xam.cpp
-	private/sparse_info2eigen_xam.cpp
-	private/sparse_low_tri_sol_xam.cpp
-	private/sparse_low2sym_xam.cpp
-	private/sparse_mat2low_xam.cpp
-	private/sparse_scale_diag_xam.cpp
-	private/sparse_up_tri_sol_xam.cpp
-	private/undetermined_xam.cpp
-	private/update_factor_xam.cpp
-	user/abs_density_xam.cpp
-	user/data_mismatch_xam.cpp
-	user/derived_ctor_xam.cpp
-	user/fix_constraint_xam.cpp
-	user/fix_likelihood_xam.cpp
-	user/information_mat_xam.cpp
-	user/lasso_xam.cpp
-	user/manage_gsl_rng_xam.cpp
-	user/no_random_xam.cpp
-	user/optimize_fixed_xam.cpp
-	user/optimize_random_xam.cpp
-	user/ran_constraint_xam.cpp
-	user/ran_likelihood_xam.cpp
-	user/ran_likelihood_hes_xam.cpp
-	user/ran_likelihood_jac_xam.cpp
-	user/sample_fixed_xam.cpp
-	user/sample_random_xam.cpp
+	private/eigen.cpp
+	private/fix_con_eval.cpp
+	private/fix_con_hes.cpp
+	private/fix_con_jac.cpp
+	private/fix_like_eval.cpp
+	private/fix_like_hes.cpp
+	private/fix_like_jac.cpp
+	private/hes_cross.cpp
+	private/ran_hes_fun.cpp
+	private/logdet_jac.cpp
+	private/newton_step.cpp
+	private/ran_con_eval.cpp
+	private/ran_con_jac.cpp
+	private/ran_like_jac.cpp
+	private/ran_obj_eval.cpp
+	private/ran_obj_jac.cpp
+	private/ran_objcon_hes.cpp
+	private/sparse_ad_cholesky_ad.cpp
+	private/sparse_eigen2info.cpp
+	private/sparse_info2eigen.cpp
+	private/sparse_low_tri_sol.cpp
+	private/sparse_low2sym.cpp
+	private/sparse_mat2low.cpp
+	private/sparse_scale_diag.cpp
+	private/sparse_up_tri_sol.cpp
+	private/undetermined.cpp
+	private/update_factor.cpp
+	user/abs_density.cpp
+	user/data_mismatch.cpp
+	user/derived_ctor.cpp
+	user/fix_constraint.cpp
+	user/fix_likelihood.cpp
+	user/information_mat.cpp
+	user/lasso.cpp
+	user/manage_gsl_rng.cpp
+	user/no_random.cpp
+	user/optimize_fixed.cpp
+	user/optimize_random.cpp
+	user/ran_constraint.cpp
+	user/ran_likelihood.cpp
+	user/ran_likelihood_hes.cpp
+	user/ran_likelihood_jac.cpp
+	user/sample_fixed.cpp
+	user/sample_random.cpp
 )
 
 SET_TARGET_PROPERTIES(

example/private/sparse_ad_cholesky_ad.cpp
@@ -1,0 +1,158 @@
+// $Id:$
+/* --------------------------------------------------------------------------
+cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
+          Copyright (C) 2014-16 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+# include <cppad/mixed/sparse_ad_cholesky.hpp>
+
+/*
+$begin sparse_ad_cholesky_ad_xam.cpp$$
+$spell
+	Cholesky
+$$
+
+$section Sparse AD Cholesky Factorization: Example and Test$$
+
+$head Problem$$
+We are given the function $latex A : \B{R}^3 \rightarrow \B{R}^{3 \times 3}$$
+defined by
+$latex \[
+	A(x) = \left( \begin{array}{ccc}
+		x_0 & 0    & x_1  \\
+		0   & x_1  & 0   \\
+		x_1 & 0    & x_2
+	\end{array} \right)
+\] $$
+The leading principal minors of this matrix are
+$latex x_0$$,
+$latex x_0 x_1$$,
+$latex x_0 ( x_1 x_2 - x_1 x_1 )$$,
+If all these minors are positive, the matrix $latex A(x)$$ is
+positive definite.
+
+$head Source$$
+$srcfile%example/private/sparse_ad_cholesky_ad.cpp
+	%4%// BEGIN C++%// END C++%1%$$
+$end
+*/
+// BEGIN C++
+bool sparse_ad_cholesky_ad_xam(void)
+{	using CppAD::AD;
+	typedef CppAD::vector<double>         d_vector;
+	typedef CppAD::vector< AD<double> >   ad_vector;
+	//
+	bool ok     = true;
+	double eps = 100. * std::numeric_limits<double>::epsilon();
+	// --------------------------------------------------------------------
+	size_t nx = 3;
+	size_t ny = 1;
+	d_vector x(nx), y(ny);
+	x[0] = 1.0;
+	x[1] = 0.5;
+	x[2] = 2.0;
+	// --------------------------------------------------------------------
+	// create sparse_ad_cholesky object
+	size_t nc = 3;
+	Eigen::SparseMatrix<double, Eigen::ColMajor> Blow(nc, nc);
+	Blow.insert(0,0) = x[0];
+	Blow.insert(2,0) = x[1];
+	Blow.insert(1,1) = x[1];
+	Blow.insert(2,2) = x[2];
+	CppAD::mixed::sparse_ad_cholesky cholesky( Blow );
+	//
+	// ----------------------------------------------------------------------
+	// Create function object corresponding to f(x)
+	//
+	// Independent variables
+	ad_vector ax(nx);
+	ax[0] = x[0];
+	ax[1] = x[1];
+	ax[2] = x[2];
+	CppAD::Independent( ax );
+	//
+	// Lower triangle of symmetric matrix with same sparsity pattern as B
+	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> aAlow(nc, nc);
+	aAlow.insert(0,0) = ax[0];
+	aAlow.insert(2,0) = ax[1];
+	aAlow.insert(1,1) = ax[1];
+	aAlow.insert(2,2) = ax[2];
+	//
+	// compute the Choleksy factorization of A
+	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> aL(nc, nc);
+	cholesky.ad(aAlow, aL);
+	//
+	// diagonal of L
+	Eigen::Matrix< AD<double> , Eigen::Dynamic , 1 > D = aL.diagonal();
+	//
+	// product of diagonal elements of L
+	AD<double> p = 1.0;
+	for(size_t j = 0; j < nc; j++)
+		p *= D[j];
+	//
+	// calculate the determinant of A using the Cholesky factorization
+	ad_vector ay(ny);
+	ay[0] = p * p;
+	//
+	// f(x) = det[ A(x) ]
+	CppAD::ADFun<double> f(ax, ay);
+	// ----------------------------------------------------------------------
+	// Test zero order forward
+	y    = f.Forward(0, x);
+	double check = x[0] * x[1] * x[2] - x[1] * x[1] * x[1];
+	ok          &= CppAD::NearEqual(y[0], check, eps, eps );
+	// -----------------------------------------------------------------------
+	// Test first order forward
+	d_vector x1(nx), y1(ny);
+	//
+	// partial w.r.t. x[0]
+	x1[0]  = 1.0;
+	x1[1]  = 0.0;
+	x1[2]  = 0.0;
+	y1     = f.Forward(1, x1);
+	double f_x0 = x[1] * x[2];
+	ok        &= CppAD::NearEqual(y1[0], f_x0, eps, eps);
+	//
+	// partial w.r.t. x[2]
+	x1[0]  = 0.0;
+	x1[2]  = 1.0;
+	y1     = f.Forward(1, x1);
+	double f_x2 = x[0] * x[1];
+	ok        &= CppAD::NearEqual(y1[0], f_x2, eps, eps);
+	//
+	// partial w.r.t. x[1]
+	x1[2]  = 0.0;
+	x1[1]  = 1.0;
+	y1     = f.Forward(1, x1);
+	double f_x1 = x[0] *  x[2] - 3.0 * x[1] * x[1];
+	ok    &= CppAD::NearEqual(y1[0], f_x1, eps, eps);
+	// -----------------------------------------------------------------------
+	// Test second order forward
+	d_vector x2(nx), y2(ny);
+	//
+	// second partial w.r.t x[1]
+	x2[0]  = 0.0;
+	x2[1]  = 0.0;
+	x2[2]  = 0.0;
+	y2     = f.Forward(2, x2);
+	double f_x11  = - 6.0 * x[1];
+	ok   &= CppAD::NearEqual(y2[0], f_x11 / 2.0, eps, eps);
+	// -----------------------------------------------------------------------
+	// Test first order reverse
+	d_vector w(ny), d1w(nx);
+	w[0] = 1.0;
+	d1w  = f.Reverse(1, w);
+	ok  &= CppAD::NearEqual(d1w[0], f_x0, eps, eps);
+	ok  &= CppAD::NearEqual(d1w[1], f_x1, eps, eps);
+	ok  &= CppAD::NearEqual(d1w[2], f_x2, eps, eps);
+	// -----------------------------------------------------------------------
+	// check for any errors during use of cholesky
+	ok &= cholesky.ok();
+	// -----------------------------------------------------------------------
+	return ok;
+}
+// END C++

src/eigen/sparse_ad_cholesky.cpp
@@ -145,7 +145,7 @@ $icode%
 and $latex A$$ is the symmetric positive definite matrix
 with lower triangle equal to $icode aAlow$$.
 
-$children%example/private/sparse_ad_cholesky_ad_xam.cpp
+$children%example/private/sparse_ad_cholesky_ad.cpp
 %$$
 $head Example$$
 The file $cref sparse_ad_cholesky_ad_xam.cpp$$ is an example

commit 19c25a88fc28ba5c5e5010dfbd0ae68e945fe0e6

Add sparse_ad_cholesky_p (permutation) documentation and test.
test_one.sh: Fix problem that started with commit dc66212d3b5.
sparse_ad_cholesky.hpp: Add notation for A, L, P.

example/CMakeLists.txt
@@ -42,6 +42,7 @@ ADD_EXECUTABLE(example EXCLUDE_FROM_ALL
 	private/ran_obj_jac.cpp
 	private/ran_objcon_hes.cpp
 	private/sparse_ad_cholesky_ad.cpp
+	private/sparse_ad_cholesky_p.cpp
 	private/sparse_eigen2info.cpp
 	private/sparse_info2eigen.cpp
 	private/sparse_low_tri_sol.cpp

example/example.cpp
@@ -68,6 +68,7 @@ extern bool ran_obj_jac_xam(void);
 extern bool sample_fixed_xam(void);
 extern bool sample_random_xam(void);
 extern bool sparse_ad_cholesky_ad_xam(void);
+extern bool sparse_ad_cholesky_p_xam(void);
 extern bool sparse_eigen2info_xam(void);
 extern bool sparse_info2eigen_xam(void);
 extern bool sparse_low_tri_sol_xam(void);
@@ -152,6 +153,7 @@ int main(void)
 	RUN(sample_fixed_xam);
 	RUN(sample_random_xam);
 	RUN(sparse_ad_cholesky_ad_xam);
+	RUN(sparse_ad_cholesky_p_xam);
 	RUN(sparse_eigen2info_xam);
 	RUN(sparse_info2eigen_xam);
 	RUN(sparse_low_tri_sol_xam);

example/private/sparse_ad_cholesky_ad.cpp
@@ -83,8 +83,10 @@ bool sparse_ad_cholesky_ad_xam(void)
 	aAlow.insert(2,2) = ax[2];
 	//
 	// compute the Choleksy factorization of A
-	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> aL(nc, nc);
+	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> aL;
 	cholesky.ad(aAlow, aL);
+	ok &= size_t(aL.rows()) == nc;
+	ok &= size_t(aL.cols()) == nc;
 	//
 	// diagonal of L
 	Eigen::Matrix< AD<double> , Eigen::Dynamic , 1 > D = aL.diagonal();

example/private/sparse_ad_cholesky_p.cpp
@@ -1,0 +1,77 @@
+// $Id:$
+/* --------------------------------------------------------------------------
+cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
+          Copyright (C) 2014-16 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+# include <cppad/mixed/sparse_ad_cholesky.hpp>
+
+/*
+$begin sparse_ad_cholesky_p.cpp$$
+$spell
+	Cholesky
+$$
+
+$section Sparse AD Cholesky Permutation: Example and Test$$
+
+$head Source$$
+$srcfile%example/private/sparse_ad_cholesky_p.cpp
+	%4%// BEGIN C++%// END C++%1%$$
+$end
+*/
+// BEGIN C++
+bool sparse_ad_cholesky_p_xam(void)
+{	using CppAD::AD;
+	//
+	bool ok        = true;
+	AD<double> eps = 100. * std::numeric_limits<double>::epsilon();
+	// --------------------------------------------------------------------
+	// create sparse_ad_cholesky object
+	int nc = 3;
+	Eigen::SparseMatrix<double, Eigen::ColMajor> Blow(nc, nc);
+	Blow.insert(0,0) = 1.0; //     [ 1.0   0.0    0.5 ]
+	Blow.insert(2,0) = 0.5; // B = [ 0.0   0.5    0.0 ]
+	Blow.insert(1,1) = 0.5; //     [ 0.5   0.0    2.0 ]
+	Blow.insert(2,2) = 2.0;
+	CppAD::mixed::sparse_ad_cholesky cholesky( Blow );
+	//
+	// Permutation matgrix kcorresponding to this cholesky
+	const Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic>& P =
+		cholesky.permutation();
+	//
+	// Lower triangle of symmetric matrix with same sparsity pattern as B
+	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> Alow(nc, nc);
+	Alow.insert(0,0) = 2.0; //     [ 2.0   0.0   0.5  ]
+	Alow.insert(2,0) = 0.5; // A = [ 0.0   0.5   0.0  ]
+	Alow.insert(1,1) = 0.5; //     [ 0.5   0.0   1.0  ]
+	Alow.insert(2,2) = 1.0;
+	//
+	// compute the Choleksy factorization of A
+	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> L;
+	cholesky.ad(Alow, L);
+	ok &= L.rows() == nc;
+	ok &= L.cols() == nc;
+	//
+	// compute P^T * L * L^T * P
+	Eigen::SparseMatrix< AD<double>, Eigen::ColMajor> tmp1, tmp2;
+	tmp1 = P.transpose() * L;
+	tmp2 = tmp1 * L.transpose();
+	tmp1 = tmp2 * P;
+	//
+	// check that A = P^T * L * L^T * P
+	Eigen::Matrix< AD<double>, 3, 3> A( Alow ), prod( tmp1 );
+	for(size_t i = 0; i < 3; i++)
+	{	for(size_t j = 0; j < 3; j++)
+		{	if( j > i )
+				A(i, j) = A(j, i);
+			ok &= CppAD::NearEqual( A(i, j), prod(i, j), eps, eps );
+		}
+	}
+	// -----------------------------------------------------------------------
+	return ok;
+}
+// END C++

include/cppad/mixed/sparse_ad_cholesky.hpp
@@ -19,6 +19,29 @@ $$
 
 $section Sparse Cholesky Factorization as an Atomic CppAD Operation$$
 
+$head Purpose$$
+Given a symmetric positive definite matrix $latex A \in \B{R}^{n \times n}$$,
+this routine computes a permutation matrix $latex P \in \B{R}^{n \times n}$$
+and a lower triangular matrix $latex L \in \B{R}^{n \times n}$$
+such that
+$latex \[
+	P A P^\R{T} = L L^\R{T}
+\] $$
+The permutation matrix is chosen to be fill reducing; i.e.,
+to make $latex L$$ sparse.
+
+$head Notation$$
+
+$subhead A$$
+We use $latex A$$ to refer to the symmetric positive definite
+matrix.
+
+$subhead P$$
+We use $latex P$$ to refer to the permutation matrix.
+
+$subhead L$$
+We use $latex L$$ to refer to the lower triangular matrix.
+
 $head Private$$
 This class is an implementation detail and not part of the
 $cref/CppAD::mixed/namespace/Private/$$ user API.
@@ -99,8 +122,7 @@ public:
 	//
 	// Permutation matrix for this factorization
 	const Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic>&
-	permutation(void)
-	{	return P_; }
+	permutation(void);
 	//
 	// AD version of atomic Cholesky factorization
 	void ad(

src/eigen/sparse_ad_cholesky.cpp
@@ -31,6 +31,7 @@ $spell
 	Alow
 	const
 	Eigen
+	Cholesky
 $$
 
 $section Sparse AD Cholesky Constructor$$
@@ -50,7 +51,8 @@ $codei%
 	const Eigen::SparseMatrix<double, Eigen::ColMajor>& %Alow%
 %$$
 and is the lower triangle of a positive definite matrix.
-Only positive definite matrices with the same sparsity pattern
+Only positive definite matrices
+$cref/A/sparse_ad_cholesky/Notation/A/$$ with the same sparsity pattern
 can be factored using the $icode cholesky$$ object; i.e.,
 square matrices with the same column size and
 same set of possibly non-zero entries.
@@ -95,6 +97,46 @@ nc_( size_t( Alow.cols() ) )
 	// L_pattern_.val.size() does not change
 }
 /*
+$begin sparse_ad_cholesky_p$$
+$spell
+	Cholesky
+	CppAD
+$$
+
+$section Using Sparse AD Cholesky Permutation P$$
+
+$head Syntax$$
+$icode%P% = %cholesky%.permutation()%$$
+
+$head Prototype$$
+$srcfile%src/eigen/sparse_ad_cholesky.cpp
+	%4%// BEGIN PERMUTATION PROTOTYPE%// END PERMUTATION PROTOTYPE%1%$$
+
+$head Public / Private$$
+This is a public member function of the class $code sparse_ad_cholesky$$.
+On the other hand, this class,
+and all of its members, are implementation details and not part of the
+$cref/CppAD::mixed/namespace/Private/$$ user API.
+
+$head P$$
+The return value is the permutation matrix
+$cref/P/sparse_ad_cholesky/Notation/P/$$.
+The permutation corresponding to $icode cholesky$$ does not change.
+
+$children%example/private/sparse_ad_cholesky_p.cpp
+%$$
+$head Example$$
+The file $cref sparse_ad_cholesky_p.cpp$$ is an example
+and test using this operation.
+
+$end
+*/
+// BEGIN PERMUTATION PROTOTYPE
+const Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic>&
+sparse_ad_cholesky::permutation(void)
+// END PERMUTATION PROTOTYPE
+{	return P_; }
+/*
 ------------------------------------------------------------------------------
 $begin sparse_ad_cholesy_ad$$
 $spell
@@ -105,7 +147,7 @@ $spell
 	Eigen
 $$
 
-$section Using Sparse AD Cholesky Operation$$
+$section Using Sparse AD Cholesky Factor L$$
 
 $head Syntax$$
 $icode%cholesky%.ad(%aAlow%, %aL%)%$$
@@ -121,7 +163,8 @@ This matrix has prototype
 $codei%
 	const Eigen::SparseMatrix< CppAD::AD<double>, Eigen::ColMajor>& %aAlow%
 %$$
-and is the lower triangle of a positive definite matrix.
+and is the lower triangle of a positive definite matrix
+$cref/A/sparse_ad_cholesky/Notation/A/$$.
 It must have the same sparsity pattern as
 $cref/Alow/sparse_ad_cholesky_ctor/Alow/$$ in the $icode cholesky$$
 constructor.
@@ -132,18 +175,9 @@ $codei%
 	Eigen::SparseMatrix< CppAD::AD<double>, Eigen::ColMajor>& %aL%
 %$$
 The input value of its elements does not matter.
-Upon return, it is a lower triangular matrix such that
-$latex \[
-	P \cdot A \cdot P^\R{T} = L \cdot L^\R{T}
-\]$$
-where $latex L$$ is the lower triangular matrix corresponding to
-$icode aL$$,
-$latex P$$ is the permutation matrix corresponding to
-$icode%
-	%P% = %cholesky%.permutation()
-%$$
-and $latex A$$ is the symmetric positive definite matrix
-with lower triangle equal to $icode aAlow$$.
+Upon return, it is a lower triangular matrix
+$cref/L/sparse_ad_cholesky/Notation/L/$$ corresponding to the matrix $latex A$$
+specified by $icode aAlow$$.
 
 $children%example/private/sparse_ad_cholesky_ad.cpp
 %$$
@@ -158,7 +192,7 @@ void sparse_ad_cholesky::ad(
 	sparse_ad_matrix&       aL     )
 {	assert( nc_ == size_t( aAlow.rows() ) );
 	assert( nc_ == size_t( aAlow.cols() ) );
-	// -----------------------------------------------------------
+	// -------------------------------------------------------------------
 	// packed version of Alow
 	size_t nx = Alow_pattern_.row.size();
 	CppAD::vector< CppAD::AD<double> > ax( nx );

bin/test_one.sh
@@ -58,7 +58,7 @@ then
 fi
 #
 file_name="$1"
-test_name=`echo $file_name | sed -e 's|.*/||' -e 's|\.cpp$||'`
+test_name=`echo $file_name | sed -e 's|.*/||' -e 's|\.cpp$|_xam|'`
 # ---------------------------------------------------------------------------
 cat << EOF > test_one.$$
 /This comment expected by bin\\/test_one.sh/b start_run