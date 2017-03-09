12375a5438d00345767aa84fd3b82d66edddbb8e
Brad Bell <bradbell@seanet.com>
2017-03-09 05:58:25 -0700

    sparse_rcv branch: optimize_fixed.cpp: use more CppAD Mixed typedefs.

--- example/user/optimize_fixed.cpp
+++ example/user/optimize_fixed.cpp
@@ -84,19 +84,21 @@ $end
 # include <cppad/mixed/cppad_mixed.hpp>
 
 namespace {
-	using CppAD::vector;
 	using CppAD::log;
 	using CppAD::AD;
-	using CppAD::mixed::sparse_mat_info;
 	//
+	using CppAD::mixed::sparse_mat_info;
 	using CppAD::mixed::a1_double;
 	using CppAD::mixed::a2_double;
-
+	using CppAD::mixed::d_vector;
+	using CppAD::mixed::a1_vector;
+	using CppAD::mixed::a2_vector;
+	//
 	class mixed_derived : public cppad_mixed {
 	private:
 		const size_t          n_fixed_;
 		const size_t          n_random_;
-		const vector<double>& y_;
+		const d_vector&       y_;
 	// ----------------------------------------------------------------------
 	public:
 		// constructor
@@ -106,7 +108,7 @@ namespace {
 			bool                   quasi_fixed   ,
 			bool                   bool_sparsity ,
 			const sparse_mat_info& A_info        ,
-			const vector<double>& y              ) :
+			const d_vector&       y              ) :
 			cppad_mixed(
 				n_fixed, n_random, quasi_fixed, bool_sparsity, A_info
 			)                     ,
@@ -118,12 +120,12 @@ namespace {
 		}
 	// ----------------------------------------------------------------------
 		// implementation of ran_likelihood
-		virtual vector<a2_double> ran_likelihood(
-			const vector<a2_double>& theta  ,
-			const vector<a2_double>& u      )
+		virtual a2_vector ran_likelihood(
+			const a2_vector&         theta  ,
+			const a2_vector&         u      )
 		{	assert( theta.size() == n_fixed_ );
 			assert( u.size() == y_.size() );
-			vector<a2_double> vec(1);
+			a2_vector vec(1);
 
 			// initialize part of log-density that is always smooth
 			vec[0] = a2_double(0.0);
@@ -148,10 +150,10 @@ namespace {
 			return vec;
 		}
 		// implementation of fix_likelihood
-		virtual vector<a1_double> fix_likelihood(
-			const vector<a1_double>& fixed_vec  )
+		virtual a1_vector fix_likelihood(
+			const a1_vector&         fixed_vec  )
 		{	assert( fixed_vec.size() == n_fixed_ );
-			vector<a1_double> vec(1);
+			a1_vector vec(1);
 
 			// initialize part of log-density that is smooth
 			vec[0] = a1_double(0.0);
@@ -172,10 +174,10 @@ namespace {
 		}
 	};
 	// derivative of objective
-	vector<double> objective_fixed(
-		const vector<double>& data   ,
-		const vector<double>& theta  )
-	{	vector<double> dF(2);
+	d_vector objective_fixed(
+		const d_vector&       data   ,
+		const d_vector&       theta  )
+	{	d_vector dF(2);
 		//
 		// compute partials of F
 		double sum   = 0.0;
@@ -203,15 +205,15 @@ bool optimize_fixed_xam(void)
 	size_t n_data   = 10;
 	size_t n_fixed  = 2;
 	size_t n_random = n_data;
-	vector<double>
+	d_vector
 		fixed_lower(n_fixed), fixed_in(n_fixed), fixed_upper(n_fixed);
 	fixed_lower[0] = - inf; fixed_in[0] = 2.0; fixed_upper[0] = inf;
 	fixed_lower[1] = .01;   fixed_in[1] = 0.5; fixed_upper[1] = inf;
 	//
 	// explicit constriants (in addition to l1 terms)
-	vector<double> fix_constraint_lower(0), fix_constraint_upper(0);
+	d_vector fix_constraint_lower(0), fix_constraint_upper(0);
 	//
-	vector<double> data(n_data), random_in(n_random);
+	d_vector data(n_data), random_in(n_random);
 	for(size_t i = 0; i < n_data; i++)
 	{	data[i]       = double(i + 1);
 		random_in[i] = 0.0;
@@ -242,7 +244,7 @@ bool optimize_fixed_xam(void)
 		"Numeric tol             1e-8\n"
 	;
 	//
-	vector<double> random_lower(n_random), random_upper(n_random);
+	d_vector random_lower(n_random), random_upper(n_random);
 	for(size_t i = 0; i < n_random; i++)
 	{	random_lower[i] = -inf;
 		random_upper[i] = +inf;
@@ -261,11 +263,11 @@ bool optimize_fixed_xam(void)
 		random_upper,
 		random_in
 	);
-	vector<double> fixed_out = solution.fixed_opt;
+	d_vector fixed_out = solution.fixed_opt;
 
 	// deriative of objective at fixed_in and fixed_out
-	vector<double> dF_in  = objective_fixed(data, fixed_in);
-	vector<double> dF_out = objective_fixed(data, fixed_out);
+	d_vector dF_in  = objective_fixed(data, fixed_in);
+	d_vector dF_out = objective_fixed(data, fixed_out);
 
 	// scaling for objective
 	double scale = std::max( std::fabs( dF_in[0] ), std::fabs( dF_in[1] ) );
