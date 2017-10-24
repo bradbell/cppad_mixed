aafcc93b2df26f9100e5cce3bd82ff08edc72cdc
Brad Bell <bradbell@seanet.com>
2017-10-24 06:36:26 -0700

    ar1_xam.sh: add type of run: normal, callgrind, or massif.

--- bin/ar1_xam.sh
+++ bin/ar1_xam.sh
@@ -8,8 +8,9 @@
 #	     GNU Affero General Public License version 3.0 or later
 # see http://www.gnu.org/licenses/agpl.txt
 # ---------------------------------------------------------------------------
+# Parameters described in ar1_xam in documentation.
 random_seed='12345'
-number_random='10000'
+number_random='1000'
 quasi_fixed='yes'
 trace_optimize_fixed='no'
 ipopt_solve='yes'
@@ -28,6 +29,13 @@ then
 	echo 'ar1_xam.sh: must first run bin/run_cmake.sh'
 	exit
 fi
+#
+if [ "$1" != 'normal' ] && [ "$1" != 'callgrind' ] && [ "$1" != 'massif' ]
+then
+	echo 'usage: bin/ar1_xam.sh (normal|callgrind|massif)'
+	exit 1
+fi
+test_to_run="$1"
 # -----------------------------------------------------------------------------
 # bash function that echos and executes a command
 echo_eval() {
@@ -37,28 +45,40 @@ echo_eval() {
 # ----------------------------------------------------------------------------
 echo_eval cd build/speed
 echo_eval make ar1_xam
-cat << EOF
-./ar1_xam \
-$random_seed \
-$number_random \
-$quasi_fixed \
-$trace_optimize_fixed \
-$ipopt_solve \
-$bool_sparsity \
-$hold_memory \
-$derivative_test \
+arguments="
+$random_seed
+$number_random
+$quasi_fixed
+$trace_optimize_fixed
+$ipopt_solve
+$bool_sparsity
+$hold_memory
+$derivative_test
 $start_near_solution
-EOF
-./ar1_xam \
-	$random_seed \
-	$number_random \
-	$quasi_fixed \
-	$trace_optimize_fixed \
-	$ipopt_solve \
-	$bool_sparsity \
-	$hold_memory \
-	$derivative_test \
-	$start_near_solution
+"
+arguments=`echo $arguments | sed -e 's|\n| |'`
+#
+if [ "$test_to_run" == 'normal' ]
+then
+	echo_eval ./ar1_xam $arguments
+fi
+if [ "$test_to_run" == 'callgrind' ]
+then
+	echo_eval valgrind \
+		--tool=callgrind \
+		--callgrind-out-file=callgrind.out.$$ \
+		./ar1_xam $arguments
+	echo "view with: kcachegrind build/speed/callgrind.out.$$"
+fi
+if [ "$test_to_run" == 'massif' ]
+then
+	echo_eval valgrind \
+		--tool=massif \
+		--massif-out-file=massif.out.$$ \
+		./ar1_xam $arguments
+	echo_eval ms_print massif.out.$$ > massif.out
+	echo "resutls are in build/speed/massif.out"
+fi
 # ----------------------------------------------------------------------------
 echo 'ar1_xam.sh: OK'
 exit 0
