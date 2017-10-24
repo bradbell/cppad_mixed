818db73118a3ccba146aee82713ae37a3bdef8c1
Brad Bell <bradbell@seanet.com>
2017-10-23 17:04:55 -0700

    ls_files.sh: remove empty path specification.

--- bin/ls_files.sh
+++ bin/ls_files.sh
@@ -2,7 +2,7 @@
 # $Id$
 #  --------------------------------------------------------------------------
 # cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-#           Copyright (C) 2014-15 University of Washington
+#           Copyright (C) 2014-16 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
@@ -22,7 +22,7 @@ then
 fi
 # -----------------------------------------------------------------------------
 list_all=`git ls-files`
-git ls-files -d "$1" > ls_files.$$
+git ls-files -d > ls_files.$$
 for file in $list_all
 do
 	pattern=`echo $file | sed -e 's|/|[/]|g' -e 's|^|^|' -e 's|$|$|'`
