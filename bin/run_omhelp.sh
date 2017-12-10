dd00dcf39b3148753d1c02abe18372afc93512e1
Brad Bell <bradbell@seanet.com>
2017-12-10 03:55:54 -0700

    Converting to using github for displaying documentation. gh_pages.sh: updates gh-pages branch. run_omhelp.sh: outputs for github display of documentation.

--- bin/run_omhelp.sh
+++ bin/run_omhelp.sh
@@ -2,7 +2,7 @@
 # $Id$
 #  --------------------------------------------------------------------------
 # cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-#           Copyright (C) 2014-15 University of Washington
+#           Copyright (C) 2014-16 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
@@ -20,60 +20,26 @@ echo_eval() {
 	echo $*
 	eval $*
 }
-if [ "$1" != 'htm' ] && [ "$1" != 'xml' ] && [ "$1" != 'all' ]
+# -----------------------------------------------------------------------------
+if [ -e 'doc' ]
 then
-	echo "usage: bin/run_omhelp.sh (htm|xml|all)"
+	echo_eval rm -r doc
+fi
+echo_eval mkdir doc
+# -----------------------------------------------------------------------------
+cd doc
+image_link='https://github.com/bradbell/cppad_mixed'
+cmd="omhelp ../doc.omh -debug -noframe -image_link $image_link"
+echo "$cmd > omhelp.log"
+if ! $cmd  > ../omhelp.log
+then
+	cat ../omhelp.log
 	exit 1
 fi
-option="$1"
-# -----------------------------------------------------------------------------
-if [ ! -e 'doc' ]
+if grep '^OMhelp Warning:' ../omhelp.log
 then
-	echo_eval mkdir doc
+	exit 1
 fi
 # -----------------------------------------------------------------------------
-version=`bin/version.sh get`
-rm -rf doc
-echo_eval mkdir -p doc/cppad_mixed-$version
-for file in `bin/ls_files.sh`
-do
-	sub_dir=`echo $file | sed -e 's|/[^/]*$||'`
-	if [ "$sub_dir" != "$file" ]
-	then
-		if [ ! -e doc/cppad_mixed-$version/$sub_dir ]
-		then
-			mkdir -p doc/cppad_mixed-$version/$sub_dir
-		fi
-	fi
-	cp $file doc/cppad_mixed-$version/$file
-done
-echo_eval cd doc
-echo_eval tar -czf cppad_mixed-$version.tgz cppad_mixed-$version
-# -----------------------------------------------------------------------------
-#
-if [ "$option" == 'htm' ]
-then
-	flag_list=':'
-elif [ "$option" == 'xml' ]
-then
-	flag_list='-xml:'
-else
-	flag_list='-xml: -xml:-printable :-printable :'
-fi
-for pair in $flag_list
-do
-	flags=`echo $pair | sed -e 's|:| |'`
-	cmd="omhelp ../doc.omh -debug -noframe $flags"
-	echo "$cmd > omhelp.log"
-	if ! $cmd  > ../omhelp.log
-	then
-		cat ../omhelp.log
-		exit 1
-	fi
-	if grep '^OMhelp Warning:' ../omhelp.log
-	then
-		exit 1
-	fi
-done
 echo 'run_omhelp.sh: OK'
 exit 0
