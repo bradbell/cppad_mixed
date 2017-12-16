8bcef6e1d1c766851779f3e19ed4c06231fc9ad0
Brad Bell <bradbell@seanet.com>
2017-12-16 03:38:24 -0700

    install_cppad.sh: Add specail case where use current cppad master.

--- bin/install_cppad.sh
+++ bin/install_cppad.sh
@@ -2,7 +2,7 @@
 # $Id:$
 #  --------------------------------------------------------------------------
 # cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-#           Copyright (C) 2014-16 University of Washington
+#           Copyright (C) 2014-17 University of Washington
 #              (Bradley M. Bell bradbell@uw.edu)
 #
 # This program is distributed under the terms of the
@@ -46,6 +46,15 @@ eval $cmd
 # cmake_libdir
 cmd=`grep '^cmake_libdir=' bin/run_cmake.sh`
 eval $cmd
+# --------------------------------------------------------------------------
+if [ "$USER" == 'bradbell' ] && [ -d $HOME/repo/cppad.git ]
+then
+	webpage="$HOME/repo/cppad.git"
+	hash_key='master'
+	pushd $webpage
+	version=`bin/version.sh get`
+	popd
+fi
 # --------------------------------------------------------------------------
 if echo "$cppad_prefix" | grep '/cppad_mixed$' > /dev/null
 then
