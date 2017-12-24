1cfb1509ea203f050e6a53f3acb6df0cb922e7be
Brad Bell <bradbell@seanet.com>
2017-12-24 05:05:49 -0700

    a3_double branch: typedef.hpp: dont repeat namespace in each definition.

--- include/cppad/mixed/typedef.hpp
+++ include/cppad/mixed/typedef.hpp
@@ -1,7 +1,7 @@
 // $Id$
 /* --------------------------------------------------------------------------
 cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-          Copyright (C) 2014-16 University of Washington
+          Copyright (C) 2014-17 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -29,26 +29,28 @@ $section Types Defined in the CppAD Mixed Namespace$$
 $head Syntax$$
 $codei%# include <cppad/mixed/typedef.hpp>
 %$$
-Note that this is not necessary if you include
-$code cppad/mixed/cppad_mixed.hpp$$.
 
+
+$head Begin Namespace$$
+All the definitions below are made inside the $code CppAD::mixed$$ namespace;
+i.e.,
+$srccode%cpp% */
+namespace CppAD { namespace mixed {
+
+/* %$$
 
 $head Scalar Types$$
 
 $subhead a1_double$$
 Scalar with one level of AD:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::AD<double> a1_double;
-} }
 /* %$$
 
 $subhead a2_double$$
 Scalar with two levels of AD:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::AD<a1_double> a2_double;
-} }
 /* %$$
 
 $head Vector Types$$
@@ -56,33 +58,25 @@ $head Vector Types$$
 $subhead s_vector$$
 Vectors with elements of type $code size_t$$:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::vector<size_t> s_vector;
-} }
 /* %$$
 
 $subhead d_vector$$
 Vectors with elements of type $code double$$:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::vector<double> d_vector;
-} }
 /* %$$
 
 $subhead a1_vector$$
 Vectors with elements of that have one level of AD:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::vector<a1_double> a1_vector;
-} }
 /* %$$
 
 $subhead a2_vector$$
 Vectors with elements of that have two levels of AD:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::vector<a2_double> a2_vector;
-} }
 /* %$$
 
 $head Sparse Types$$
@@ -90,28 +84,26 @@ $head Sparse Types$$
 $subhead sparse_rc$$
 Sparsity patterns using index vector of type $code s_vector$$:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::sparse_rc<s_vector> sparse_rc;
-} }
 /* %$$
 
 $subhead sparse_rcv$$
 Sparse matrices using index vector of type $code s_vector$$
 and value vectors of type $code d_vector$$:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::sparse_rcv<s_vector, d_vector> sparse_rcv;
-} }
 
 /* %$$
 $subhead a1_sparse_rcv$$
 Sparse matrices using index vector of type $code s_vector$$
 and value vectors of type $code a1_vector$$:
 $srccode%cpp% */
-namespace CppAD { namespace mixed {
 	typedef CppAD::sparse_rcv<s_vector, a1_vector> a1_sparse_rcv;
-} }
+/* %$$
 
+$head End Namespace$$
+$srccode%cpp% */
+} }
 /* %$$
 $end
 */
