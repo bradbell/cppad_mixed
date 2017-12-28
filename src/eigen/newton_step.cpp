510510d36ff7c7173f14c42712d0a3c950a2b40d
Brad Bell <bradbell@seanet.com>
2017-12-28 04:30:59 -0700

    subgraph branch: newton_step.cpp: edit specification for hes_rcv.

--- src/eigen/newton_step.cpp
+++ src/eigen/newton_step.cpp
@@ -1,7 +1,7 @@
 // $Id:$
 /* --------------------------------------------------------------------------
 cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-          Copyright (C) 2014-16 University of Washington
+          Copyright (C) 2014-17 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -62,9 +62,10 @@ $code%
 %$$
 It is the
 $cref/sparse_rcv/typedef/Sparse Types/sparse_rcv/$$
-information for the Hessian with respect to the
-random effects (as a function of the fixed and random effects); i.e.
+information for the lower triangle of the Hessian with respect to the
+random effects, as a function of the fixed and random effects; i.e.
 $latex f_uu ( \theta , u)$$.
+In addition, this matrix is in column major order.
 
 $head hes_work$$
 This argument has prototype
