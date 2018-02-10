374587489e2aaa99b79bdd5c03b410056ba958ad
Brad Bell <bradbell@seanet.com>
2018-02-10 04:32:59 -0700

    Extract eval_h.cpp from ipopt_fixed.cpp.

--- include/cppad/mixed/ipopt_fixed.hpp
+++ include/cppad/mixed/ipopt_fixed.hpp
@@ -63,6 +63,7 @@ $childtable%src/ipopt_fixed.cpp
 	%src/ipopt_fixed/eval_grad_f.cpp
 	%src/ipopt_fixed/eval_g.cpp
 	%src/ipopt_fixed/eval_jac_g.cpp
+	%src/ipopt_fixed/eval_h.cpp
 	%example/ipopt_xam.omh
 %$$
 
