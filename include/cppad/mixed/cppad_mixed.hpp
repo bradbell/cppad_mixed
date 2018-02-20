9578ad2fb08c98ffd052c078517342039c41396c
Brad Bell <bradbell@seanet.com>
2018-02-20 06:06:27 -0700

    cppad_mixed.hpp: Fix some friend declarations.

--- include/cppad/mixed/cppad_mixed.hpp
+++ include/cppad/mixed/cppad_mixed.hpp
@@ -1,7 +1,7 @@
 // $Id$
 /* --------------------------------------------------------------------------
 cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-          Copyright (C) 2014-17 University of Washington
+          Copyright (C) 2014-18 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -24,7 +24,14 @@ see http://www.gnu.org/licenses/agpl.txt
 # include <cppad/mixed/fixed_solution.hpp>
 # include <cppad/mixed/typedef.hpp>
 
-// private examples
+/*
+Examples and tests that use cppad_mixed private information.
+This list can be generated using the following command:
+grep -P '^\t*friend *bool *::' cppad_mixed.hpp | \
+	sed -e 's|^\t*friend *bool *::|extern bool |' | sort
+*/
+extern bool delta_ran_obj(void);
+extern bool der_var_hes(void);
 extern bool fix_con_eval_xam(void);
 extern bool fix_con_hes_xam(void);
 extern bool fix_con_jac_xam(void);
@@ -32,21 +39,19 @@ extern bool fix_like_eval_xam(void);
 extern bool fix_like_hes_xam(void);
 extern bool fix_like_jac_xam(void);
 extern bool hes_cross_xam(void);
-extern bool ran_hes_fun_xam(void);
+extern bool laplace_obj_hes(void);
+extern bool laplace_obj_hes_xam(void);
 extern bool logdet_jac_xam(void);
 extern bool ran_con_eval_xam(void);
 extern bool ran_con_jac_xam(void);
+extern bool ran_hes_fun_xam(void);
+extern bool ran_jac_fun_xam(void);
 extern bool ran_like_jac_xam(void);
 extern bool ran_obj_eval_xam(void);
 extern bool ran_obj_jac_xam(void);
-extern bool laplace_obj_hes_xam(void);
-extern bool update_factor_xam(void);
-
-//  tests
-extern bool der_var_hes(void);
-extern bool delta_ran_obj(void);
-extern bool sample_fixed_1(void);
 extern bool ran_obj_tst(void);
+extern bool sample_fixed_1(void);
+extern bool update_factor_xam(void);
 
 
 namespace CppAD { namespace mixed {
