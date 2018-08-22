b5548756fb2a33b9cda8d7e42a56380122432fac
Brad Bell <bradbell@seanet.com>
2018-08-22 05:42:51 -0700

    master: ldlt_eigen: make eigen_sparse and eigen_ldlt private.

--- include/cppad/mixed/ldlt_eigen.hpp
+++ include/cppad/mixed/ldlt_eigen.hpp
@@ -1,7 +1,7 @@
 // $Id:$
 /* --------------------------------------------------------------------------
 cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
-          Copyright (C) 2014-16 University of Washington
+          Copyright (C) 2014-18 University of Washington
              (Bradley M. Bell bradbell@uw.edu)
 
 This program is distributed under the terms of the
@@ -87,10 +87,10 @@ $end
 namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 
 class ldlt_eigen {
-public:
+private:
 	typedef Eigen::SparseMatrix<double, Eigen::ColMajor>      eigen_sparse;
 	typedef Eigen::SimplicialLDLT<eigen_sparse, Eigen::Lower> eigen_ldlt;
-private:
+	//
 	const size_t    n_row_;
 	eigen_ldlt*     ptr_;
 	//
