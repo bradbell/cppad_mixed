fdca67a95222ec4f81eb70dc17518bfca34e8837
Brad Bell <bradbell@seanet.com>
2018-08-30 08:48:48 -0700

    master: speed_new.sh: make more like speed_branch and speed_date.

--- bin/speed_new.sh
+++ bin/speed_new.sh
@@ -14,35 +14,24 @@ then
 	echo 'bin/speed_new.sh must be run from its parent directory'
 	exit 1
 fi
-test2run='normal'
-quasi_fixed='yes'
-while [ "$1" != '' ]
-do
-	if [ "$1" == '--callgrind' ]
-	then
-		if [ "$test2run" == 'massif' ]
-		then
-			echo 'usage: speed_new.sh: cant specify both callgrind and massif'
-			exit 1
-		fi
-		test2run='callgrind'
-	elif [ "$1" == '--massif' ]
-	then
-		if [ "$test2run" == 'callgrind' ]
-		then
-			echo 'usage: speed_new.sh: cant specify both callgrind and massif'
-			exit 1
-		fi
-		test2run='massif'
-	elif [ "$1" == '--newton' ]
-	then
-		quasi_fixed='no'
-	else
-		echo 'usage: bin/speed_new.sh: [--callgrind] [--massif] [--newton]'
-		exit 1
-	fi
-	shift
-done
+ok='yes'
+if [ "$1" != 'normal' ] && [ "$1" != 'massif' ] && [ "$1" != 'callgrind' ]
+then
+	ok='no'
+fi
+if [ "$2" != 'yes' ] && [ "$2" != 'no' ]
+then
+	ok='no'
+fi
+if [ "$ok" == 'no' ]
+then
+	echo 'usage: bin/speed_new.sh test2run quasi_fixed'
+	echo 'test2run:    is normal, massif, or callgrid'
+	echo 'quasi_fixed: is yes or no'
+	exit 1
+fi
+test2run="$1"
+quasi_fixed="$2"
 # -----------------------------------------------------------------------------
 # bash function that echos and executes a command
 echo_eval() {
@@ -56,9 +45,8 @@ then
 fi
 # -----------------------------------------------------------------------------
 git reset --hard
-bin/run_cmake.sh --release
+bin/run_cmake.sh --release --optimize_cppad_function
 # -----------------------------------------------------------------------------
-# set random seed to 123 so same for old and new
 for program in ar1_xam capture_xam
 do
 	diff=`git diff bin/$program.sh`
@@ -67,54 +55,43 @@ do
 		echo "bin/speed_new.sh: bin/$program.sh has changed"
 		exit 1
 	fi
-	git checkout bin/$program.sh
-	sed -i bin/$program.sh -e "s|^quasi_fixed=.*|quasi_fixed=$quasi_fixed|"
-	for ext in old new
+	for ver in old new
 	do
-		if [ -e "build/$program.$ext" ]
+		if [ -e "build/$ver.$program" ]
 		then
-			echo_eval rm build/$program.$ext
+			echo_eval rm build/$ver.$program
 		fi
 	done
 done
 # -----------------------------------------------------------------------------
 # old version of code
-for program in ar1_xam capture_xam
+for ver in old new
 do
-	cd build; make $program; cd ..
-	echo "bin/$program.sh $test2run > build/$program.old"
-	bin/$program.sh $test2run > build/$program.old
-	if [ "$test2run" == 'massif' ]
-	then
-		mv build/speed/massif.out build/massif.old
-		sed -i build/$program.old \
-			-e 's|build/speed/massif.out|build/massif.old|'
-	fi
-done
-# -----------------------------------------------------------------------------
-# new version of code
-git_new.sh from
-for program in ar1_xam capture_xam
-do
-	cd build; make $program; cd ..
-	echo "bin/$program.sh $test2run > build/$program.new"
-	bin/$program.sh $test2run > build/$program.new
-	if [ "$test2run" == 'massif' ]
-	then
-		mv build/speed/massif.out build/massif.new
-		sed -i build/$program.new \
-			-e 's|build/speed/massif.out|build/massif.new|'
-	fi
-done
-# -----------------------------------------------------------------------------
-# restore random seed
-for program in ar1_xam capture_xam
-do
-	git checkout bin/$program.sh
+	for program in ar1_xam capture_xam
+	do
+		# make sure quasi_fixed is as expected
+		sed -i bin/$program.sh \
+			-e "s|^quasi_fixed=.*|quasi_fixed=$quasi_fixed|"
+		#
+		cd build; make $program; cd ..
+		cp build/speed/$program build/$ver.$program
+		#
+		echo "bin/$program.sh $test2run > build/$ver.$program.out"
+		bin/$program.sh $test2run > build/$ver.$program.out
+		if [ "$test2run" == 'massif' ]
+		then
+			mv build/speed/massif.out build/$ver.massif.out
+			sed -i build/$ver.$program.out \
+				-e 's|build/speed/massif.out|build/massif.out|'
+		fi
+		git checkout bin/$program.sh
+	done
+	# This may overwrite bin/$program.sh
+	git_new.sh from
 done
 # -----------------------------------------------------------------------------
 echo 'speed_new.sh: results are in'
-echo 'build/ar1_xam.old,     build/ar1_xam.new'
-echo 'build/capture_xam.old, build/capture_xam.new'
+echo 'build/old.ar1_xam.out,     build/new.ar1_xam.out'
+echo 'build/old.capture_xam.out, build/new.capture_xam.out'
 echo 'speed_new.sh: OK'
 exit 0
