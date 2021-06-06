82da855ca6d31edfc03046e33e778502739adeb0
Brad Bell <bradbell@seanet.com>
2021-06-05 19:29:56 -0700

    master: working on return ipopt trace information.

--- include/cppad/mixed/ipopt_fixed.hpp
+++ include/cppad/mixed/ipopt_fixed.hpp
@@ -87,6 +87,9 @@ namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 		typedef Ipopt::Number               Number;
 		typedef Ipopt::Index                Index;
 		typedef Ipopt::TNLP::IndexStyleEnum IndexStyleEnum;
+		typedef Ipopt::AlgorithmMode               AlgorithmMode;
+		typedef Ipopt::IpoptData                   IpoptData;
+		typedef Ipopt::IpoptCalculatedQuantities   IpoptCalculatedQuantities;
 		// ---------------------------------------------------------------
 		// member variables set during constructor
 		//
@@ -320,22 +323,22 @@ namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 			cppad_mixed&              mixed_object
 		);
 		//
-		virtual bool get_nlp_info(
+		bool get_nlp_info(
 			Index&          n            ,
 			Index&          m            ,
 			Index&          nnz_jac_g    ,
 			Index&          nnz_h_lag    ,
 			IndexStyleEnum& index_style
-		);
-		virtual bool get_bounds_info(
+		) override;
+		bool get_bounds_info(
 				Index       n        ,
 				Number*     x_l      ,
 				Number*     x_u      ,
 				Index       m        ,
 				Number*     g_l      ,
 				Number*     g_u
-		);
-		virtual bool get_starting_point(
+		) override;
+		bool get_starting_point(
 			Index           n            ,
 			bool            init_x       ,
 			Number*         x            ,
@@ -345,27 +348,27 @@ namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 			Index           m            ,
 			bool            init_lambda  ,
 			Number*         lambda
-		);
-		virtual bool eval_f(
+		) override;
+		bool eval_f(
 			Index           n        ,
 			const Number*   x        ,
 			bool            new_x    ,
 			Number&         obj_value
-		);
-		virtual bool eval_grad_f(
+		) override;
+		bool eval_grad_f(
 			Index           n        ,
 			const Number*   x        ,
 			bool            new_x    ,
 			Number*         grad_f
-		);
-		virtual bool eval_g(
+		) override;
+		bool eval_g(
 			Index           n        ,
 			const Number*   x        ,
 			bool            new_x    ,
 			Index           m        ,
 			Number*         g
-		);
-		virtual bool eval_jac_g(
+		) override;
+		bool eval_jac_g(
 			Index           n        ,
 			const Number*   x        ,
 			bool            new_x    ,
@@ -374,8 +377,8 @@ namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 			Index*          iRow     ,
 			Index*          jCol     ,
 			Number*         values
-		);
-		virtual bool eval_h(
+		) override;
+		bool eval_h(
 			Index         n              ,
 			const Number* x              ,
 			bool          new_x          ,
@@ -387,8 +390,8 @@ namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 			Index*        iRow           ,
 			Index*        jCol           ,
 			Number*       values
-		);
-		virtual void finalize_solution(
+		) override;
+		void finalize_solution(
 			Ipopt::SolverReturn               status    ,
 			Index                             n         ,
 			const Number*                     x         ,
@@ -400,7 +403,22 @@ namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
 			Number                            obj_value ,
 			const Ipopt::IpoptData*           ip_data   ,
 			Ipopt::IpoptCalculatedQuantities* ip_cq
-		);
+		) override;
+		bool intermediate_callback(
+			AlgorithmMode               mode,
+			Index                       iter,
+			Number                      obj_value,
+			Number                      inf_pr,
+			Number                      inf_du,
+			Number                      mu,
+			Number                      d_norm,
+			Number                      regularization_size,
+			Number                      alpha_du,
+			Number                      alpha_pr,
+			Index                       ls_trials,
+			const IpoptData*            ip_data,
+			IpoptCalculatedQuantities*  ip_cq
+		) override;
 		// -----------------------------------------------------------------
 		bool adapt_derivative_chk( bool trace, double relative_tol);
 	};
---
+++ src/ipopt_fixed/intermediate_callback.cpp
@@ -0,0 +1,128 @@
+/* --------------------------------------------------------------------------
+cppad_mixed: C++ Laplace Approximation of Mixed Effects Models
+          Copyright (C) 2014-21 University of Washington
+             (Bradley M. Bell bradbell@uw.edu)
+
+This program is distributed under the terms of the
+	     GNU Affero General Public License version 3.0 or later
+see http://www.gnu.org/licenses/agpl.txt
+-------------------------------------------------------------------------- */
+# include <cppad/mixed/ipopt_fixed.hpp>
+
+namespace CppAD { namespace mixed { // BEGIN_CPPAD_MIXED_NAMESPACE
+/* %$$
+-------------------------------------------------------------------------------
+$begin ipopt_fixed_intermediate_callback$$
+$spell
+	Ipopt
+	iter
+	obj
+	inf
+	pr
+	du
+	ls
+	ip_cq
+	lg
+	rg
+	optimizer
+	mu
+	enum
+$$
+
+$section Get Optimizer Trace Information From Ipopt$$
+
+$head Syntax$$
+$icode%ok% = intermediate_callback(
+	%mode%,
+	%iter%,
+	%obj_value%,
+	%inf_pr%,
+	%inf_du%,
+	%mu%,
+	%d_norm%,
+	%regularization_size%,
+	%alpha_du%,
+	%alpha_pr%,
+	%ls_trials%,
+	%ip_data%,
+	%ip_cq%
+)%$$
+
+$head mode$$
+This is an $code enum$$ value and equal to
+$code RegularMode$$ or $code RestorationPhaseMode$$.
+
+$head iter$$
+See ipopt trace $cref/iter/ipopt_trace/iter/$$.
+
+$head obj_value$$
+See ipopt trace $cref/objective/ipopt_trace/objective/$$.
+
+$head inf_pr$$
+See ipopt trace $cref/inf_pr/ipopt_trace/inf_pr/$$.
+
+$head inf_du$$
+See ipopt trace $cref/inf_du/ipopt_trace/inf_du/$$.
+
+$head mu$$
+See ipopt trace $cref/lg(mu)/ipopt_trace/lg(mu)/$$.
+
+$head d_norm$$
+See ipopt trace $cref/||d||/ipopt_trace/||d||/$$.
+
+$head regularization_size$$
+See ipopt trace $cref/lg(rg)/ipopt_trace/lg(rg)/$$.
+
+$head alpha_du$$
+See ipopt trace $cref/alpha_du/ipopt_trace/alpha_du/$$.
+
+$head alpha_pr$$
+See ipopt trace $cref/alpha_pr/ipopt_trace/alpha_pr/$$.
+
+$head ls_trials$$
+See ipopt trace $cref/ls/ipopt_trace/ls/$$.
+
+$head ok$$
+If set to false, the optimizer will terminate with
+$code USER_REQUESTED_STOP$$ as the finalize_solution
+$cref/status/ipopt_xam_finalize_solution/status/$$.
+
+$head Source$$
+$srccode@cpp@ */
+bool ipopt_fixed::intermediate_callback(
+	AlgorithmMode               mode                 ,   // in
+	Index                       iter                 ,   // in
+	Number                      obj_value            ,   // in
+	Number                      inf_pr               ,   // in
+	Number                      inf_du               ,   // in
+	Number                      mu                   ,   // in
+	Number                      d_norm               ,   // in
+	Number                      regularization_size  ,   // in
+	Number                      alpha_du             ,   // in
+	Number                      alpha_pr             ,   // in
+	Index                       ls_trials            ,   // in
+	const IpoptData*            ip_data              ,   // in
+	IpoptCalculatedQuantities*  ip_cq                )   // in
+{
+/*
+	size_t size = solution_.trace_vec.size();
+	assert( size == size_t(iter) );
+	trace_struct trace;
+	//
+	trace.iter                = size_t(iter);
+	trace.obj_value           = obj_value;
+	trace.inf_pr              = inf_pr;
+	trace.inf_du              = inf_du;
+	trace.mu                  = mu;
+	trace.d_norm              = d_norm;
+	trace.regularization_size = regularization_size;
+	trace.alpha_du            = alpha_du;
+	trace.alpha_pr            = alpha_pr;
+	trace.ls_trials           = size_t(ls_trials);
+	//
+	solution_.trace_vec.push_back(trace);
+*/
+	return true;
+}
+
+} } // END_CPPAD_MIXED_NAMESPACE
